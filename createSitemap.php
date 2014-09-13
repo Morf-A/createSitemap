a6c7b6751b2e1e0cd8e8b53688971a07dd9eeb49
Morf-A <Morf-a@yandex.ru>
2014-09-13 18:46:18 +0400

    Преобразуем json в массив uri

--- createSitemap.php
+++ createSitemap.php
@@ -6,7 +6,7 @@ $login    = 'demo';
 $password = '123';
 
 // Получаем json-карту сайта
-$jsonMap = file_get_contents(
+$jsonSiteMap = file_get_contents(
     $host,
     false,
     stream_context_create([
@@ -18,4 +18,29 @@ $jsonMap = file_get_contents(
     ])
 );
 
-echo $jsonMap;
+
+$baseUrl = 'http://site.ru/';
+//$uriList = [];
+
+$arraySiteMap = json_decode($jsonSiteMap);
+
+function createUriList($tempSiteMap, $path){
+    $uriList = [];
+    foreach ($tempSiteMap as $parent => $child) {
+
+        if(is_string($child)){
+            $uriList[] = $path . $child . '.html';
+        }else{
+            $newPath = $path . $parent . '/';
+            $uriList[] = $newPath;
+            $uriList = array_merge($uriList, createUriList($child, $newPath));
+        }
+    }
+    return $uriList;
+}
+
+
+$uriList = array_merge([$baseUrl], createUriList($arraySiteMap, $baseUrl));
+
+print_r($uriList);
+
